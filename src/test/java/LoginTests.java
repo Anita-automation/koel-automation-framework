00490ab2fc4d1553626f4208b57693103a45d413
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-25 12:50:13 +0100

    f
    f

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,8 +10,34 @@ public class LoginTests extends BaseTest {
     public void loginValidEmailPassword() {
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
-            loginPage.provideEmail("anita.surewicz@testpro.io").providePassword("AnitaAnita1029").clickSubmit();
+
+        loginPage.provideEmail("anita.surewicz@testpro.io")
+                .providePassword("AnitaAnita1029")
+                .clickSubmit();
 
         Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+    }
+
+    @Test
+    public void loginInvalidEmailValidPassword() {
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        loginPage.provideEmail("dfaf@gmail.com")
+                .providePassword("AnitaAnita1029")
+                .clickSubmit();
+
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+    }
+
+    @Test
+    public void loginValidEmailEmptyPassword() {
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.provideEmail("anita.surewicz@testpro.io")
+                .providePassword("")
+                .clickSubmit();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa.koel.app/");
     }
 }
