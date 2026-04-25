2cab972869a8d6fdc7a5d632cd8b21a45b249161
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-25 16:04:14 +0100

    .

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -5,32 +5,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage {
+
     public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
+    // Existing locator
     By userAvatarIcon = By.cssSelector("img.avatar");
 
-    public WebElement getUserAvatar () {
+    public WebElement getUserAvatar() {
         return findElement(userAvatarIcon);
     }
-}
-By playNextSongBtn = By.xpath("//i[@role='button' and @title='Play next song']");
-By playBtn = By.xpath("//span[@role='button' and @title='Play or resume']");
-By soundBar = By.xpath("//img[@alt='Sound bars']");
 
-}
+    // NEW LOCATORS
+    By playNextSongBtn = By.xpath("//i[@role='button' and @title='Play next song']");
+    By playBtn = By.xpath("//span[@role='button' and @title='Play or resume']");
+    By soundBar = By.xpath("//img[@alt='Sound bars']");
 
-public void clickPlayNextSong() {
-    findElement(playNextSongBtn).click();
-}
+    // NEW METHODS
+    public void clickPlayNextSong() {
+        findElement(playNextSongBtn).click();
+    }
 
-public void clickPlayButton() {
-    findElement(playButton).click();
-}
+    public void clickPlayButton() {
+        findElement(playBtn).click();
+    }
 
-public boolean isSoundBarVisible() {
-    return findElement(soundBar).isDisplayed();
-}
-
+    public boolean isSoundBarVisible() {
+        return findElement(soundBar).isDisplayed();
+    }
 }
