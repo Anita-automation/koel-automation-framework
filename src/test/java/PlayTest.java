7595a3a5f6c901f2c1ec3a4bb7a44ed57f6cde9b
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-27 11:45:28 +0100

    .

--- src/test/java/PlayTest.java
+++ src/test/java/PlayTest.java
@@ -17,6 +17,7 @@ public class PlayTest extends BaseTest{
 
         HomePage homePage = new HomePage(driver);
         homePage.waitUntilLoaded();
+        dsfasfasas
 
         // Interact with the player using HomePage methods
         homePage.hoverOverPlayer();
