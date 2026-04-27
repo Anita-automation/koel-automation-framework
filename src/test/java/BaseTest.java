cdf142b9cc7c199343e52d1711f519e9c587d721
AnitaAnita10 <anita.surewicz@testpro.io>
2026-04-27 16:16:59 +0100

    hijhk

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -14,33 +14,36 @@ import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
-public class BaseTest {
+public class BaseTest { //starts the class
 
-    public static WebDriver driver;
-    public static String url;
-    public static WebDriverWait wait = null;
+    public static WebDriver driver; //This creates a browser variable that all tests can use. It will hold ChromeDriver once the browser is launched.
+    public static String url; //This stores the website address (BaseURL) that your tests will open.
+    //gets this info from From your testng.xml file, where you define the parameter. <suite name="MySuite">
+    //    <parameter name="BaseURL" value="https://qa.koel.app/"/>
+    public static WebDriverWait wait = null; //creates a wait helper, filled in befor methods
     public static Actions actions;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
-    }
+    } //Download the correct ChromeDriver version for this machine
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowser(String BaseURL) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+    public void launchBrowser(String BaseURL) { //this method receives the BaseURL from your TestNG XML file.
+        ChromeOptions options = new ChromeOptions(); //creates object so can customize it later
+        options.addArguments("--remote-allow-origins=*"); //adds setting to chrome - allows remote origins
         options.addArguments("--disable-notifications");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = new ChromeDriver(options); //starts chrome with the set options
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //allows the wait, automatically
         driver.manage().window().maximize();
-        url = BaseURL;
+        url = BaseURL; //Stores the BaseURL (from XML) into your url variable.
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40)); //wait mentioned as null in base test is initialized
+        //can call on this if needed
 
         actions = new Actions(driver);
-        navigateToPage();
+        navigateToPage(); //opens the page used the stored URL
     }
 
     @AfterMethod
